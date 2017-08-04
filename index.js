db00ca77dd326fcd5884476eb1f1de0d17a310c8
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-08-04 16:26:38 -0400

    Use spaces and strict

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+'use strict';
 module.exports = function objectMock (obj, propertyKey, newValue) {
-	var oldValue = obj[propertyKey];
-	obj[propertyKey] = newValue;
-	return function unmock () {
-		obj[propertyKey] = oldValue;
-	};
+  var oldValue = obj[propertyKey];
+  obj[propertyKey] = newValue;
+  return function unmock () {
+    obj[propertyKey] = oldValue;
+  };
 }
